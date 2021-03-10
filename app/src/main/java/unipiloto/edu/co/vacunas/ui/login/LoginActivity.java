e9e44f3b16ef7f39a04dace168444491cd53b57e
cristian-avendano <cristian-avendano@upc.edu.co>
2021-03-09 21:27:02 -0500

    Fix activity error

--- app/src/main/java/unipiloto/edu/co/vacunas/ui/login/LoginActivity.java
+++ app/src/main/java/unipiloto/edu/co/vacunas/ui/login/LoginActivity.java
@@ -14,50 +14,40 @@ import unipiloto.edu.co.vacunas.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText email;
-    EditText password;
-    Button registrar;
-    Button ingresar;
+    private Button registrar;
+    private Button ingresar;
+    private EditText email;
+    private EditText password;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        intiComponents();
         setup();
-
     }
 
-
-
-    public void setup(){
+    public void  intiComponents(){
         registrar = findViewById(R.id.registro);
         ingresar = findViewById(R.id.ingresar);
         email = findViewById(R.id.email);
         password = findViewById(R.id.Password);
+    }
 
+    public void setup(){
         registrar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                email.getText();
-
-                enviar(email.getText().toString());
+                System.out.println(email.getText());
+                System.out.println(password.getText());
+                enviar(v, email.getText().toString());
             }
         });
-        ingresar.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-
     }
 
-    public void enviar(String mensaje){
-        Intent  intent = new Intent(this, Prueba.class);
+    public void enviar(View v, String mensaje){
+        Intent intent = new Intent(this, Prueba.class);
         intent.putExtra(Prueba.mensaje,mensaje);
-        System.out.println("si entraaaaaa");
+        startActivity(intent);
     }
-
-
 }
